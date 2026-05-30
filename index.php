6881357748c25896a4c6258aa24387f1a4d3b413
jasiel333 <jasieldumana951@gmail.com>
2026-05-30 23:53:59 +0800

    same sh* nothing changes wt

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ while ($row = mysqli_fetch_assoc($cat_result)) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>IT-Festival Invitational Run 2026<</title>
+    <title>IT-Festival Invitational Run 2026</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@300;400;500;600;700&family=Barlow+Condensed:wght@400;600;700;800&display=swap" rel="stylesheet">
     <style>
