86bff96ee0abad976b5dbc4c574c43f150de6351
jasiel333 <jasieldumana951@gmail.com>
2026-05-30 10:41:31 +0800

    added navbar

---
+++ includes/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+
+    <a href="index.php" class="logo">
+        RUN <span>WITH</span> JASIEL & JHON
+    </a>
+
+    <div class="nav-links">
+        <a href="index.php">Home</a>
+        <a href="participants.php">Participants</a>
+        <a href="register.php?category=10KM" class="btn-register">Register Now</a>
+    </div>
+
+</nav>
